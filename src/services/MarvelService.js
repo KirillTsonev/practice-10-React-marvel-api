73904e1844780ccfd1bf66a1e960259f613ad9e2
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-09-24 05:06:34 +0300

    Info commit

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -22,16 +22,15 @@ class MarvelService {
         return this._transformCharacter(res.data.results[0])
     }
 
-    //http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg
-    //http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
-
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
+            comics: char.comics.items,
         }
     }
 }
